774fc4956da90914f80fb7e138102ff60666de11
Joshua Briggs <gitlab@urbanwarzonepaintball.com>
2025-08-21 09:56:27 -0500

    No material changes, code readability and clippy checks

--- api/src/types/session_sweeper.rs
+++ api/src/types/session_sweeper.rs
@@ -13,9 +13,7 @@ impl SessionSweeper {
 
         let _garbage_collector = actix_web::rt::spawn(async move {
             match app_state.sessions() {
-                SessionControllerStatus::Enabled(controller) => {
-                    controller.watch().await
-                },
+                SessionControllerStatus::Enabled(controller) => controller.watch().await,
                 SessionControllerStatus::Disabled => {}
             }
         });
