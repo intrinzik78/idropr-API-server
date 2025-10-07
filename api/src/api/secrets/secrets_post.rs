856247600087ca8aea0804f31da4b1ee596158e7
Joshua Briggs <gitlab@urbanwarzonepaintball.com>
2025-10-07 07:21:53 -0500

    Adds open api spec for secrets and sessions

--- api/src/api/secrets/secrets_post.rs
+++ api/src/api/secrets/secrets_post.rs
@@ -1,13 +1,14 @@
 use actix_web::{web::{Data,Json}, Responder};
 use serde::Deserialize;
+use utoipa::ToSchema;
 
 use crate::{
     enums::Error,
     types::{ApiResponse, AppState, secrets::DecryptedSecret, permissions::WereChecked}
 };
 
-#[derive(Deserialize)]
-pub struct Post {
+#[derive(Debug,Deserialize,ToSchema)]
+pub struct CreateSecretBody {
     name: String,
     description: String,
     api_key: Option<String>,
@@ -17,13 +18,13 @@ pub struct Post {
 pub struct SecretsPost;
 
 impl SecretsPost {
-    pub async fn logic(_permissions: WereChecked, post: Json<Post>, shared: Data<AppState>) -> impl Responder {
+    pub async fn logic(_permissions: WereChecked, post: Json<CreateSecretBody>, shared: Data<AppState>) -> impl Responder {
         let controller = shared.secrets();
         let database = shared.database();
         let secret = DecryptedSecret::new(&post.name, &post.description, &post.api_key, &post.api_secret);
 
         match controller.new_secret(secret, database).await {
-            Ok(_) => ApiResponse::success(),
+            Ok(()) => ApiResponse::resource_created().ok(),
             Err(Error::DuplicateSecretNameExists) => ApiResponse::bad_request().with_message("duplicate: api name already in use".to_string()).error(),
             Err(e) => {
                 // add log here
