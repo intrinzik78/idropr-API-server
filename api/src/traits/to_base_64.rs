aa63cc46eea2573cd11eb1c124f741102d70359a
Joshua Briggs <gitlab@urbanwarzonepaintball.com>
2025-10-07 07:14:05 -0500

    Implements ToHash for str and String
    Converts a str / String to blake3::Hash

--- api/src/traits/to_base_64.rs
+++ api/src/traits/to_base_64.rs
@@ -1,5 +1,7 @@
 use base64::prelude::*;
 
+use crate::enums::Uuid;
+
 /// converts a token buffer to base64 for http transport
 pub trait ToBase64 {
     fn to_base64_url(&self) -> String;
@@ -14,5 +16,21 @@ impl ToBase64 for [u8;32] {
 impl ToBase64 for &[u8;32] {
     fn to_base64_url(&self) -> String {
         BASE64_URL_SAFE_NO_PAD.encode(self)
+    }
+}
+
+impl ToBase64 for String {
+    fn to_base64_url(&self) -> String {
+        BASE64_URL_SAFE_NO_PAD.encode(self)
+    }
+}
+
+impl ToBase64 for Uuid {
+    fn to_base64_url(&self) -> String {
+        match self {
+            Uuid::Crypto(bytes) => BASE64_URL_SAFE_NO_PAD.encode(bytes),
+            Uuid::WebSafe(s) => BASE64_URL_SAFE_NO_PAD.encode(s),
+            Uuid::WebSafeNums(s) => BASE64_URL_SAFE_NO_PAD.encode(s)
+        }
     }
 }
