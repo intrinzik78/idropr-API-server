0ed1cf83e4774c7d18d784ed4e36abdabf9e8993
Joshua Briggs <gitlab@urbanwarzonepaintball.com>
2025-11-14 19:52:24 -0600

    Reserves Locations placeholder filenames

--- api/src/types/users/mod.rs
+++ api/src/types/users/mod.rs
@@ -1,7 +1,9 @@
+mod builder;
 mod business_user;
 mod community_user;
 mod system_user;
 
+pub use builder::Builder;
 pub use business_user::BusinessUser;
 pub use community_user::CommunityUser;
 pub use system_user::SystemUser;
